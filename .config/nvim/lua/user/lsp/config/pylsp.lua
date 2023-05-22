26a4504255c2bb1bafb0e93da665588d68641309
Denis Yantarev <denis.yantarev@gmail.com>
2023-05-22 13:12:25 +0100

    astronvim config

---
+++ .config/nvim/lua/user/lsp/config/pylsp.lua
@@ -0,0 +1,18 @@
+return {
+  settings = {
+    pylsp = {
+      plugins = {
+        flake8 = {
+          enabled = true,
+          maxLineLength = 99,
+        },
+        pycodestyle = {
+          enabled = false,
+        },
+        pyflakes = {
+          enabled = false,
+        },
+      }
+    }
+  }
+}
