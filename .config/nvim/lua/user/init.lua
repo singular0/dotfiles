c2064e68e01eb465845d984c15834f5b0668e237
Denis Yantarev <denis.yantarev@gmail.com>
2023-06-01 22:03:42 +0100

    System copy/paste keys map

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -1,9 +1,18 @@
--- Allow clipboard copy paste in neovim
+-- Allow clipboard copy paste with macOS key combinations
 vim.g.neovide_input_use_logo = 1
-vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+
+vim.api.nvim_set_keymap('n', '<D-c>', '"*yy', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<D-v>', '"*p',  { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<D-x>', '"*dd', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('i', '<D-v>', '<C-R>*', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('v', '<D-c>', '"*y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<D-v>', '"*P', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<D-x>', '"*d', { noremap = true, silent = true })
+
+-- Toggle terminal to normal mode with Esc
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
 
 return {
   lsp = {
