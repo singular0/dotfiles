59369ba05ba8b05723adf0b656ecb2efeeaf974b
Denis Yantarev <denis.yantarev@gmail.com>
2023-10-28 12:43:45 +0100

    Fix .zshrc bootstrap

--- .config/yadm/bootstrap.d/oh-my-zsh.sh
+++ .config/yadm/bootstrap.d/oh-my-zsh.sh
@@ -7,7 +7,7 @@ ZSHRC_BACKUP="$HOME/.zshrc.pre-oh-my-zsh"
 if [ ! -d "$ZSH" ]; then
   echo "Bootstrapping oh-my-zsh"
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-  if [ ! -f "$ZSHRC_BACKUP" ]; then
+  if [ -f "$ZSHRC_BACKUP" ]; then
     echo "Restoring $ZSHRC"
     mv "$ZSHRC_BACKUP" "$ZSHRC"
   fi
