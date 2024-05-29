d8bdfd9e37fc2688cd1c63130b7636e205403d11
Denis Yantarev <denis.yantarev@gmail.com>
2024-05-29 22:38:56 +0100

    ChatGPT API key on Linux

--- .config/nvim/lua/plugins/chatgpt.lua
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -8,7 +8,15 @@ return {
     },
     event = "VeryLazy",
     opts = function(_, opts)
-      opts.api_key_cmd = "security find-generic-password -l openai_api_key -w"
+      opts.api_key_cmd = vim.fn.has("macunix")
+        and "security find-generic-password -l openai_api_key -w"
+        or  "pass show openai_api_key"
+      opts.openai_params = {
+        model = "gpt-4o"
+      }
+      opts.openai_edit_params = {
+        model = "gpt-4o"
+      }
       return opts
     end,
   },
