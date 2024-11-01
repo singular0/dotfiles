e6d0217e3f69b9e4aab1fb8d1a6dba6a06668aa5
Denis Yantarev <denis.yantarev@gmail.com>
2024-11-01 10:03:41 +0000

    Add openrouter.ai config to avante

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -3,13 +3,67 @@ return {
     "yetone/avante.nvim",
     event = "VeryLazy",
     lazy = false,
+    version = false,
     opts = {
       behaviour = {
         auto_suggestions = false,
       },
+      provider = "openrouter",
+      vendors = {
+        ---@type AvanteProvider
+        openrouter = {
+          endpoint = "https://openrouter.ai/api/v1",
+          model = "anthropic/claude-3.5-sonnet",
+          api_key_name = "OPENROUTER_API_KEY",
+          ---@type fun(opts: AvanteProvider, code_opts: AvantePromptOptions): AvanteCurlOutput
+          parse_curl_args = function(opts, code_opts)
+            return {
+              url = opts.endpoint .. "/chat/completions",
+              headers = {
+                ["Authorization"] = "Bearer " .. opts.parse_api_key(),
+                ["Content-Type"] = "application/json",
+              },
+              body = {
+                model = opts.model,
+                messages = require("avante.providers").openai.parse_message(code_opts),
+                stream = true,
+              },
+            }
+          end,
+          ---@type fun(data_stream: string, event_state: string, opts: ResponseParser): nil
+          parse_response_data = function(data_stream, event_state, opts)
+            require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+          end,
+        },
+        ---@type AvanteProvider
+        ollama = {
+          ["local"] = true,
+          endpoint = "http://localhost:11434/api",
+          model = "llama3.2",
+          ---@type fun(opts: AvanteProvider, code_opts: AvantePromptOptions): AvanteCurlOutput
+          parse_curl_args = function(opts, code_opts)
+            return {
+              url = opts.endpoint .. "/chat",
+              headers = {
+                ["Content-Type"] = "application/json",
+              },
+              body = {
+                model = opts.model,
+                messages = require("avante.providers").copilot.parse_message(code_opts),
+                stream = true,
+              },
+            }
+          end,
+          ---@type fun(data_stream: string, event_state: string, opts: ResponseParser): nil
+          parse_response_data = function(data_stream, event_state, opts)
+            require("avante.providers").openai.parse_response(data_stream, event_state, opts)
+          end,
+        },
+      },
     },
     build = "make",
     dependencies = {
+      "nvim-treesitter/nvim-treesitter",
       "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
