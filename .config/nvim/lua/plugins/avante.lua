c2fe0a3d2fe0156042c0ecfb90d0b78c6440e2b2
Denis Yantarev <denis.yantarev@gmail.com>
2025-06-24 16:30:16 +0100

    Update avante config

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -4,14 +4,17 @@
 return {
   "yetone/avante.nvim",
   opts = {
-    provider = "openrouter",
+    provider = "claude_sonnet_4",
+    behaviour = {
+      enable_cursor_planning_mode = true,
+    },
     providers = {
-      openrouter = {
-        __inherited_from = 'openai',
+      claude_sonnet_4 = {
+        __inherited_from = "openai",
         api_key_name = "OPENROUTER_API_KEY",
         endpoint = "https://openrouter.ai/api/v1",
         model = "anthropic/claude-sonnet-4",
-      }
+      },
     },
   },
 }
