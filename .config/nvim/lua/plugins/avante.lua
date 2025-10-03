866267b6a0c7947b13af66e88e8b49684d013578
Denis Yantarev <denis.yantarev@gmail.com>
2025-10-04 00:05:13 +0100

    Rename LLM providers

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -4,16 +4,37 @@
 return {
   "yetone/avante.nvim",
   opts = {
-    provider = "claude_sonnet_4",
+    provider = "openrouter/grok-code-fast-1",
     behaviour = {
-      enable_cursor_planning_mode = true,
+      enable_fastapply = true,
     },
     providers = {
-      claude_sonnet_4 = {
+      ["morph"] = {
+        __inherited_from = "openai",
+        api_key_name = "OPENROUTER_API_KEY",
+        endpoint = "https://openrouter.ai/api/v1",
+        model = "morph/morph-v3-large",
+      },
+      ["openrouter/grok-code-fast-1"] = {
+        __inherited_from = "openai",
+        api_key_name = "OPENROUTER_API_KEY",
+        endpoint = "https://openrouter.ai/api/v1",
+        model = "x-ai/grok-code-fast-1",
+        max_tokens = 256000,
+      },
+      ["openrouter/claude-sonnet-4"] = {
         __inherited_from = "openai",
         api_key_name = "OPENROUTER_API_KEY",
         endpoint = "https://openrouter.ai/api/v1",
         model = "anthropic/claude-sonnet-4",
+        max_tokens = 200000,
+      },
+      ["openrouter/qwen3-coder"] = {
+        __inherited_from = "openai",
+        api_key_name = "OPENROUTER_API_KEY",
+        endpoint = "https://openrouter.ai/api/v1",
+        model = "qwen/qwen3-coder",
+        max_tokens = 262000,
       },
     },
   },
